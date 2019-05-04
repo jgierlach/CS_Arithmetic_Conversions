ac63c3aecc4c1dfece2b0296326d8a164f332994
jgierlach <jegierlach@gmail.com>
2019-05-04 09:55:15 -0400

    conversion to ieee754 now works

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,9 +4,24 @@
       <div class="center white-background padding move-down z-depth-4">
         <h3>
           I built this site because I wanted create something using Vue.js. I thought a resource like this would be helpful to students in intro level Comp Sci classes. If you're interested in seeing the algorithms I wrote to do these operations you can find them
-          <a href="https://github.com/jgierlach/conversion_scripts" target="_blank">here</a>. If you have any questions or feedback feel free to reach out to me.
+          <a href="https://github.com/jgierlach/conversion_scripts" target="_blank">here</a>. If you have any questions or feedback feel free to <a @click="contact">reach out</a> to me.
         </h3>
       </div>
     </transition>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    contact() {
+      this.$router.push('/contact')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
