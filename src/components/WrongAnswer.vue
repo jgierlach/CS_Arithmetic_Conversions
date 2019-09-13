c47b85691266773c22870156862a7ee05bf24eb5
jgierlach <jegierlach@gmail.com>
2019-09-13 18:46:57 -0400

    Switched language to try again

--- src/components/WrongAnswer.vue
+++ src/components/WrongAnswer.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="row container center red move-down padding z-depth-4">
-    <h3 class="white-text">Wrong!</h3>
+    <h3 class="white-text">Try Again!</h3>
   </div>
 </template>
